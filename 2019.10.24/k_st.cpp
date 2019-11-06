b638eb2c9e9cea9251d27ce202a9316f41c45133
MarryAnt <47720071+MarryAnt@users.noreply.github.com>
2019-11-06 15:07:24 +0300

    Create k_st.cpp

---
+++ 2019.10.24/k_st.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int k_st(int arr[], int l, int r, int k){
+    if(l >= r){
+        return arr[l];
+    }
+    int i = l;
+    int j = r;
+    int p = arr[(l+r)/2];
+    while(i < j){
+        while(arr[i] < p){
+            i++;
+        }
+        while(arr[j] > p){
+            j--;
+        }
+        if(i < j){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+    if((k >= l) && (k < i)){
+        return k_st(arr, l, i-1, k);
+    }
+    else if((j < k) && (k <= r)){
+        return k_st(arr, j+1, r, k);
+    }
+    else{
+        return p;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    cout << k_st(arr, 0, n-1, k-1);
+    return 0;
+}
